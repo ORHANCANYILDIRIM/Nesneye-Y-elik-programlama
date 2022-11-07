bf1a4e1d35947ea0953d31d495391a813bff4e43
ORHANCANYILDIRIM <99863519+ORHANCANYILDIRIM@users.noreply.github.com>
2022-11-08 00:03:12 +0300

    Orhancan Yıldırım
    02210224036

---
+++ labwiewer.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package labviewer;
+import javax.swing.JFrame;
+import java.io.*;
+
+
+public class labwiewer {
+
+    
+   
+    public static void main(String[] args) {
+      JFrame frame = new fileframe();
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setVisible(true);
+    }
+    
+}
